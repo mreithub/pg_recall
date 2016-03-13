ca6cb0dc6fdcb62019c9ea5d018cef907554dbc5
Manuel Reithuber <git@manuel.reithuber.net>
2016-03-13 18:40:17 +0100

    updated sql/crud.sql: added view_config_log (avoids code duplication)

--- sql/crud.sql
+++ sql/crud.sql
@@ -16,6 +16,10 @@ CREATE TABLE config (
 -- enable pg_recall and configure it to store data for three months
 SELECT recall.enable('config', '3 months');
 
+-- create a helper view to query the log table
+CREATE VIEW view_config_log AS
+SELECT key, value, now() - _log_start AS _start, now() - _log_end AS _end FROM recall.config_log ORDER BY _log_start, key;
+
 -- insert a few values
 INSERT INTO config (key, value) VALUES ('enable_something', 'true');
 INSERT INTO config (key, value) VALUES ('some_number', '42');
@@ -25,7 +29,7 @@ INSERT INTO config (key, value) VALUES ('some_number', '42');
 --  - _start is '@ 0' in both cases
 --  - _end is null
 SELECT key, value FROM config ORDER BY key;
-SELECT key, value, now() - _log_start AS _start, now() - _log_end AS _end FROM recall.config_log ORDER BY _log_start, key;
+SELECT * FROM view_config_log;
 
 
 -- update value (to work around the now() issue manually set the log values back one hour)
@@ -40,7 +44,7 @@ UPDATE config SET value = 'false' WHERE key = 'enable_something';
 --  - start is '@ 0' for the new log entry and '1 hour' for the others
 --  - end is '@ 0' for the old 'enable_something' value and NULL for all the others
 SELECT key, value FROM config ORDER BY key;
-SELECT key, value, now() - _log_start AS _start, now() - _log_end AS _end FROM recall.config_log ORDER BY _log_start, key;
+SELECT * FROM view_config_log;
 
 
 -- do a bulk key update (the equivalent to deleting all entries and creating new ones)
@@ -48,7 +52,7 @@ SELECT pretendToWait('1 hour');
 UPDATE config SET key = key||'_';
 
 SELECT key, value FROM config ORDER BY key;
-SELECT key, value, now() - _log_start AS _start, now() - _log_end AS _end FROM recall.config_log ORDER BY _log_start, key;
+SELECT * FROM view_config_log;
 
 
 -- delete an entry (again after pushing log entries back one hour)
@@ -56,11 +60,11 @@ SELECT pretendToWait('1 hour');
 DELETE FROM config WHERE key = 'some_number_';
 
 SELECT key, value FROM config ORDER BY key;
-SELECT key, value, now() - _log_start AS _start, now() - _log_end AS _end FROM recall.config_log ORDER BY _log_start, key;
-
+SELECT * FROM view_config_log;
 
 -- query the log table for the current state (for an easy way to compare it to the data table)
-SELECT key, value FROM recall.config_log WHERE _log_start <= now() AND (_log_end IS NULL OR _log_end > now()) ORDER BY key;
+SELECT recall.at('config', now());
+SELECT key, value FROM config_past ORDER BY key;
 SELECT key, value FROM config ORDER BY key;
 
 -- query the log table for the state of one hour ago:
